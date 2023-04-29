51314ab82fba914e754c39fb6f1b6265ac6e9750
developer006 <developer.ludovic@gmail.com>
2023-04-29 04:12:42 +0300

    add beautifull login and registrationpage

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ use App\Http\Controllers\BabyProgressHealthReportController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
