fc72eef1a0e2af4e898fbdba28d67e7ac530ca4b
developer006 <developer.ludovic@gmail.com>
2023-07-12 11:11:19 +0300

    fix all

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-{{--<nav class="navbar navbar-expand navbar-light navbar-white">
+<nav class="navbar navbar-expand navbar-light  bg-teal">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,101 +14,10 @@
         </ul>
 
         <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav mr-auto">
             <!-- Authentication Links -->
-            @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @endguest
+           <h3>CLINIC MANAGEMENT SYSTEM</h3>
+
         </ul>
     </div>
-</nav>--}}
-
-
-
-
-
-
-
-
-
-
-
-
-<nav class="main-header navbar navbar-expand navbar-light bg-teal">
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
-                <i class="icon ion-md-menu"></i>
-            </a>
-        </li>
-        {{-- <li class="nav-item d-none d-sm-inline-block">
-            <form class="form-inline ml-3">
-                <div class="input-group input-group-md">
-                    <input class="form-control form-control-navbar text-dark" type="search" placeholder="Search" aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </li> --}}
-    </ul>
-
-    <ul class="navbar-nav ml-auto">
-
-        <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
-
-
-
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <span>
-                    <img src="{{asset('assets/images/avatar.png')}}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                </span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-envelope mr-2"></i>Profile
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-users mr-2"></i>Change Password
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-file mr-2"></i>Logout
-                </a>
-            </div>
-        </li>
-
-    </ul>
 </nav>
